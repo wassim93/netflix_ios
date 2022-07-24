7dd58965616668914739ae009ef2883f0c14d1bf
wassim <b.wassim1993@hotmail.fr>
2022-07-24 22:57:49 +0100

    initial commit

--- netflix_clone/MainTabViewController.swift
+++ netflix_clone/MainTabViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemYellow
         // Do any additional setup after loading the view.
     }
 

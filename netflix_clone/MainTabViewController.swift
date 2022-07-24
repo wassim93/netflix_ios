c6194f7981b2d7a46a0467c6db173e741017217e
wassim <b.wassim1993@hotmail.fr>
2022-07-24 23:56:42 +0100

    init project setup

--- netflix_clone/MainTabViewController.swift
+++ netflix_clone/MainTabViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class MainTabViewController: UIViewController {
+class MainTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = Constants.HOME
+        vc2.title = Constants.COMING
+        vc3.title = Constants.SEARCH
+        vc4.title = Constants.DOWNLOADS
+        tabBar.tintColor = .label
+
+
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
+
         // Do any additional setup after loading the view.
     }
 
---
+++ netflix_clone/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  netflix_clone
+//
+//  Created by wassim on 24/7/2022.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let HOME = "Home"
+    static let COMING = "Coming Soon"
+    static let SEARCH = "Top Search"
+    static let DOWNLOADS = "Downloads"
+
+}

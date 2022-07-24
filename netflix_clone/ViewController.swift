2d3e3f619202539a169c5eed62dadf8eb13b4c8a
wassim <b.wassim1993@hotmail.fr>
2022-07-24 22:36:27 +0100

    Initial Commit

---
+++ netflix_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix_clone
+//
+//  Created by wassim on 24/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
